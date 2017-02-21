63cb0e871a8242f895ce7baed05abb421e03b935
Gangeshwar Krishnamurthy <gangeshwark@gmail.com>
2017-02-21 10:52:56 +0800

    Minor changes

--- QbE_System.py
+++ QbE_System.py
@@ -45,14 +45,14 @@ def upload_audio():
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            #flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            '''
+
             filename = secure_filename(file.filename)
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            #file.save(path)
-            path = '/projects/PycharmProjects/QbE_System/uploads/hello1.wav'
+            file.save(path)
+
             query_features_path = FeatureExtractor.bnf(path)
             corpus_features_path = os.path.abspath('../corpus_features/bnf_database/raw_bnfea_fbank_pitch.1.scp')
             print corpus_features_path, query_features_path
@@ -60,29 +60,9 @@ def upload_audio():
             AQS = model_bnf.AQSearch(query_features_path, corpus_features_path)
 
             matrix, top_k = AQS.search()
-            '''
-            top_k = np.array([[10, 2, 3],
-                                [20, 3, 4],
-                                [30, 3, 4],
-                                [40, 5, 6],
-                                [60, 5, 6],
-                                [80, 5, 6],
-                                [100, 5, 6],
-                                [120, 5, 6],
-                                [140, 5, 6],
-                                [160, 5, 6],
-                                [180, 5, 6],
-                                [200, 5, 6],
-                                [220, 5, 6],
-                                [240, 5, 6],
-                                [260, 5, 6],
-                                [280, 5, 6],
-                                [300, 5, 6],
-                                [320, 5, 6],
-                                [340, 5, 6],
-                                [360, 5, 6]])
+
             print type(top_k)
-            return json.dumps(top_k.tolist())
+            return json.dumps(top_k)
 
 
 @app.route('/static/<path:path>')
